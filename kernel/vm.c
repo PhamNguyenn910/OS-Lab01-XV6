3ed809f7ae9bac9bdf08aca1a6caf7eee9822824
duongnam2847-dotcom <duongnam2847@gmail.com>
2026-04-20 18:53:33 +0700

    Implement vmprint and vmprint_helper functions
    Add vmprint and vmprint_helper functions to print page table details.

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,26 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+static void vmprint_helper(pagetable_t pagetable, int depth) {
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if (!(pte & PTE_V))
+      continue;
+
+    for (int d = 0; d <= depth; d++)
+      printf(".. ");
+
+    uint64 pa = PTE2PA(pte);
+    printf("%d: pte %p pa %p\n", i, (void*)pte, (void*)pa);  // ← cast both to void*
+
+    if ((pte & (PTE_R | PTE_W | PTE_X)) == 0) {
+      vmprint_helper((pagetable_t)pa, depth + 1);
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  vmprint_helper(pagetable, 0);
+}
